53807db4c7a506f26bb10cea3738fe66a41a97dc
harshsd <hdeshpande5998@gmail.com>
2018-06-06 07:42:45 +0530

    Newly added svm_5_fold_test not checked

--- svm_test.py
+++ svm_test.py
@@ -5,6 +5,61 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def accuracy_svm (data_set1 , data_set2 , cc , gg):
+	accuracy = []
+	l = len(data_set1)
+	fatigue_train1 = []
+	fatigue_train2 = []
+	fatigue_test1 = []
+	fatigue_test2 = []
+	if l != len (data_set2):
+		print ("error")
+		print (l)
+		print (len(data_set2))
+	for i in range (0,5):
+		fatigue_trial1 = []
+		fatigue_trial2 = []
+		fatigue_test1 = []
+		fatigue_test2 = []		
+		test1 = data_set1[int(i*l/5):int((i+1)*l/5)]
+		test2 = data_set2[int(i*l/5):int((i+1)*l/5)]
+		if i == 0:
+			train1 = data_set1[l/5:l]
+			train2 = data_set2[l/5:l]
+		elif i == 4 :
+			train1 = data_set1[0:4*l/5]
+			train2 = data_set2[0:4*l/5]	
+		else:
+			train1 = data_set1[0:i*l/5]
+			train1 += data_set1[i*l/5:l]
+			train2 = data_set2[0:i*l/5]
+			train2 += data_set2[i*l/5:l]
+		test1 += test2
+		train1 += train2	
+		for k in range (0,int(4*l/5)):
+			fatigue_train1.append(0)
+			fatigue_train2.append(1)
+			if(k<int(l/5)):
+				fatigue_test1.append(0)
+				fatigue_test2.append(1)	
+		fatigue_train1 += fatigue_train2
+		fatigue_test1 += fatigue_test2 		
+		clf = svm.SVC(kernel='rbf', C=cc, gamma = gg )				
+		clf.fit (train1 , fatigue_train1)
+		fatigue_result = clf.predict(test1)
+		for i in range (0,len(fatigue_result)):
+			if (fatigue_result[i] == fatigue_test[i]):
+				y=y+1
+			else:
+				n=n+1
+		accuracy.append (float(y/y+n))
+	accuracy = np.array(accuracy)
+	mean = accuracy.mean()
+	std = accuracy.std()
+	return (mean , std)			
+
+
 #Get features in this part  should be an array of arrays or list of list with each small list containing features of one segment and many such lists
 # Assumed final list os lists is in entropies_train[[]]
 #For each list give in array fatigue =1 and not-fatigue=0
@@ -12,8 +67,8 @@ import matplotlib.pyplot as plt
 
 mean_old = 0
 for entropy in ["permut"]:#,"tsallis","renyi","permut"]:	
-	for ccc in range (1,100):
-		for ggg in range (1,100):
+	for ccc in range (1,10):
+		for ggg in range (1,10):
 			cc = ccc/10
 			gg = ggg/10
 			print (cc,gg)
@@ -21,12 +76,11 @@ for entropy in ["permut"]:#,"tsallis","renyi","permut"]:
 			std_accuracies = []
 			#print (entropy)
 			accuracy = []
+			accuracy_std = []
 			for sub in range (1,26):
 				for turn in range (1,3):
-					entropies_train = []
-					entropies_test = []
-					fatigue_train = []
-					fatigue_test = []
+					data_set1 = []
+					data_set2 = []
 					for ch in range (1,25):
 							os.chdir ('/media/harsh/DATA/Readable_Data/Entropies')
 							file_name = entropy+"s"+str(sub)+"t"+str(turn)+"c"+str(ch)+".txt"
@@ -41,78 +95,24 @@ for entropy in ["permut"]:#,"tsallis","renyi","permut"]:
 									#print (a)
 									#print (word)
 									temp.append(str(float(word)))
-							f.close()		
-							#print (temp)		
-							temp2 = temp[0:300] 
-							temp2 += temp[len(temp)-300:len(temp)]
-							temp3 = temp[300:400]
-							temp3 += temp[len(temp)-400:len(temp)-300]
-							#temp3 = temp2
-							# print(len (temp3))
-							entropies_train.append(temp2)	
-							entropies_test.append(temp3)	
-							#print (len(temp2))
-							# print (len(entropies_train))
-
-					#print (entropies_train[0])
-					entropies_train = np.array(entropies_train)
-					entropies_test = np.array(entropies_test)
-					# print(entropies_train.shape)
-					entropies_train = entropies_train.transpose()
-					entropies_test = entropies_test.transpose()
-					#print (entropies_train.shape)
-					# print(len(entropies_test[0]))
-					for i in range (0,300):
-						fatigue_train.append(0)
-					for j in range (0,300):
-						fatigue_train.append(1)	
-					for k in range (0,100):               #CHANGE_HERE!!!!!!!!!
-						fatigue_test.append(0)
-					for l in range (0,100):
-						fatigue_test.append(1)		
-
-					#print (entropies_train[0])
-
-
-					#accuracy = 0.0
-					#Apply SVM  
-					#Best yet c=0.01 gamma=0.646 75.5  0.1 0.52 76
-					# for c in range (1,10):
-					# 	for gamma in range (1,1000):
-					# 		gg = gamma/100
-					# 		cc = c/10
-
-					# cc = 0.1
-					# gg = 0.52
-					clf = svm.SVC(kernel='rbf', C=cc, gamma = gg )
-					clf.fit(entropies_train,fatigue_train)  
-
-
-					#test the result
-					fatigue_result = clf.predict(entropies_test)
-					y=0
-					n=0
-					for i in range (0,len(fatigue_result)):
-						if (fatigue_result[i] == fatigue_test[i]):
-							y=y+1
-						else:
-							n=n+1
-							#print (fatigue_test[i])
-					#accuracy_new = 0.00		
-					accuracy_new = float((y/(y+n)))*100		
-			# if (accuracy_new > accuracy):
-			# 	accuracy = accuracy_new
-			# 	print (cc,gg)
-			# 	print (y)
-			# 	print (n)
-			# 	print (accuracy)
-					accuracy.append(accuracy_new)
-					# os.chdir('/media/harsh/DATA/Readable_Data/Results_unfiltered/SVM')
-					# writing = open ("svm_results.txt","a+")
-					# writing.write("renyi \n")
-					# writing.write(str(accuracy_new))
-					# writing.write("\n")
-					# writing.close()
+							f.close()				
+							data1 = temp[0:300]
+							data2 = temp[len(temp)-300:len(temp)]
+							data_set1.append(data1)
+							data_set2.append(data2)
+					data_set1 = np.array(data_set1)
+					x , y = data_set1.shape
+					if (y!=24):
+						print ("error")
+						print (y)
+						print (x)
+					data_set1 = data_set1.reshape((24,x))
+					data_set2 = np.array(data_set2)
+					x , y = data_set2.shape
+					data_set2 = data_set2.reshape(y,x)
+					mean , std = accuracy_svm(data_set1 , data_set2 , cc , gg)
+					accuracy.append(mean)
+					accuracy_std.append(std)
 			accuracy = np.array(accuracy)				
 			mean = accuracy.mean()
 			if (mean > mean_old):
